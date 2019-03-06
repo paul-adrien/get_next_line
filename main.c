992de3e1c02d27de9e9535d1f4c76dae2efbe233
Paul-adrien LAURENT <plaurent@e2r8p20.42.fr>
2019-03-06 11:04:32 +0100

    oui

---
+++ main.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plaurent <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/29 12:35:18 by plaurent          #+#    #+#             */
+/*   Updated: 2019/03/04 16:16:09 by plaurent         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	free(line);
+	if (argc == 2)
+		close(fd);
+	return (0);
+}
